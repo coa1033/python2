bcb9234681c60ee7c96a788c8c990c215c72ea92
coa1033 <condingcoa1033@gmail.com>
2022-03-15 21:40:24 +0900

    day01

--- day01/study03.py
+++ day01/study03.py
@@ -10,3 +10,13 @@ dic["3월"] = 31
 print(dic["1월"])
 print(dic["2월"])
 print(dic["3월"])
+
+
+# 문제 : 딕셔너리에 각 달의 마지막 날들을 한번에 담고, keys()로 key 순회출력
+
+dic1 = {}
+dic1_list = [31,28,31,30,31,30,31,31,30,31,30,31]
+for i , j in enumerate(dic1_list):
+    month = str(i+1) + "월"
+    dic1[month] = j
+print(dic1.keys())
